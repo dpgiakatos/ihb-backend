95e77a5a8ea595e1987e86edeee1b7da9cf6349e
Dimitrios Panteleimon Giakatos <dgiakatos@csd.auth.gr>
2020-03-15 15:46:06 +0200

    JWT works

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,27 @@
-import {Controller, Request, Post, UseGuards, Body} from '@nestjs/common';
-import {AuthGuard} from "@nestjs/passport";
+import {Controller, Request, Post, UseGuards, Body, Get} from '@nestjs/common';
+import {AuthService} from "./auth.service";
 import {UsersService} from "../users/users.service";
+import {LocalAuthGuard} from "./local-auth.guard";
+import {JwtAuthGuard} from "./jwt-auth.guard";
 
 @Controller('auth')
 export class AuthController {
-    constructor(private usersService: UsersService) {}
+    constructor(private usersService: UsersService, private authService: AuthService) {}
 
-    @UseGuards(AuthGuard('local'))
+    @UseGuards(LocalAuthGuard)
     @Post('login')
     async login(@Request() req) {
-        return req.user;
+        return this.authService.login(req.user);
     }
 
     @Post('register')
     async register(@Body() newUser) {
         await this.usersService.create(newUser);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+        return req.user;
     }
 }
